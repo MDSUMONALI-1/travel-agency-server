4e3572b2553c1e245215b280451244759dde15e6
MDSUMONALI-1 <mdsumonaliceng@gmail.com>
2021-10-31 21:56:15 +0600

    four

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ const port = process.env.PORT || 6000;
 app.use(cors());
 app.use(express.json());
 
-const uri = "mongodb+srv://aliceng:CPYGPkH0idj7KPLX@cluster0.aq2lr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aq2lr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
+console.log(uri)
 async function run() {
     try {
         await client.connect();
